80e781cc21b9e817c4cf102dc6dd3c9bcd36c1c0
slimtv <vizzio.bugsreport@gmail.com>
2015-08-29 01:16:46 +0700

    Create fragment.js

---
+++ fragment.js
@@ -0,0 +1,69 @@
+
+	var web;
+	
+	function onCreateView(){
+			Fragment.setHasOptionsMenu(true);
+			Fragment.getMainActivity().getSupportActionBar().show();
+			return web = Fragment.asWebPanel();
+	}
+	
+	function onViewCreated(){
+		Fragment.browsable(true);
+		web.loadUrl("http://google.com");
+	}
+	
+	function onBrowseSubmit(query){
+		web.loadUrl(query);
+	}
+	
+	function onRefresh(webview, url){
+		web.reload();
+		Fragment.openFragment("/a");
+	}
+	
+	function onWebPageFinished(webview, url){
+		var js = "(function(){															\
+			function getScript(url, success) {	 										\
+				if(window.jQuery != null){												\
+					success();															\
+					return;																\
+				}																		\
+				if( typeof url === 'string' ) {											\
+					url = [ url ];														\
+				}																		\
+				var script, 															\
+					head = document.getElementsByTagName('head')[0];					\
+				for(var i = 0;i < url.length; i++){										\
+					script = document.createElement('script');							\
+					script.src = url[i];												\
+					head.appendChild(script);											\
+				}																		\
+				var done = false;														\
+				script.onload = script.onreadystatechange = function() {				\
+					if (!done && (!this.readyState || this.readyState == 'loaded' 		\
+								 || this.readyState == 'complete')) {					\
+						done = true;													\
+						success();														\
+						script.onload = script.onreadystatechange = null;				\
+						head.removeChild(script);										\
+					}																	\
+				};																		\
+			}																			\
+																						\
+			getScript([	'http://code.jquery.com/jquery-latest.min.js', 					\
+						'https://raw.githubusercontent.com/cowboy/jquery-replacetext/master/jquery.ba-replacetext.min.js'],\
+						function() {													\
+				jQuery(document).ready(function($){										\
+					Fragment.onWebCallback('jQuery ready');								\
+					function callback( str ){											\
+						return '<b>' + str + '<\/b>';									\
+					};																	\
+					$('body *').replaceText( /a/gi, callback );							\
+				});																		\
+			});																			\
+			return 'js executed';														\
+		})()";
+		
+		Fragment.log(js);
+		web.evaluateJavascript(js, function(s){Fragment.log(s)});
+	}
